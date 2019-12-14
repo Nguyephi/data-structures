add2a7379cbbd8a5f7879f6e1e3df7166a349a56
Phil <pronewgen92@gmail.com>
2019-12-14 14:06:34 -0500

    set complete.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this.storage[item] === undefined) this.storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  var isPresent = false;
+  for (var key in this.storage) {
+    if (this.storage[key] === true) {
+      isPresent = true
+    }
+  }
+  return isPresent;
 };
 
 setPrototype.remove = function(item) {
+  for (var key in this.storage) {
+    if (this.storage[key] === true) {
+      var temp = this.storage[key]
+      delete this.storage[key]
+      return temp;
+    }
+  }
 };
 
 /*
